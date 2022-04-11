9ac2cc2e18d68c0b9be98bada837bbf0ccb72f95
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-04-11 20:45:16 +0530

    Update about.py

--- bot/plugins/about.py
+++ bot/plugins/about.py
@@ -15,27 +15,23 @@ BUTTONS = [[
 ABOUT_TEXT = """
 ╭────[🔅Sᴄʀᴇᴇɴsʜᴏᴛ Bᴏᴛ🔅]───⍟
 │
-├<b>🤖 Bot Name : <a href='https://t.me/TeleRoid_Screenshot_Bot'>@TeleRoid_Screenshot_Bot</a></b>
+├<b>Bot Name: <a href='https://t.me/mmscreenshotbot'>@mmscreenshotbot</a></b>
 │
-├<b>📢 Channel : <a href='https://t.me/TeleRoidGroup'>@TeleRoidGroup</a></b>
+├<b>Channel: <a href='https://t.me/mm_dev_channel'>Dev Channel</a></b>
 │
-├<b>♂️ Support: <a href='https://t.me/TeleRoid14'>@TeleRoidSupport</a></b>
+├<b>Support: <a href='https://t.me/TeleRoid14'>@TeleRoidSupport</a></b>
 │
-├<b>👥 Version : <a href='https://t.me/TeleRoid_Screenshot_Bot'>0.9.2 beta</a></b>
+├<b>Source: <a href='https://github.com/PredatorHackerzZ'>Click Here</a></b>
 │
-├<b>🛠 Source : <a href='https://github.com/PredatorHackerzZ'>Click Here</a></b>
+├<b>Server: <a href='https://heroku.com'>Heroku</a></b>
 │
-├<b>🌐 Server : <a href='https://heroku.com'>Heroku</a></b>
+├<b>Library: <a href='https://github.com/pyrogram'>Pyrogram 1.2.8</a></b>
 │
-├<b>📕 Library : <a href='https://github.com/pyrogram'>Pyrogram 1.2.8</a></b>
+├<b>Language: <a href='https://www.python.org'>Python 3.9.4</a></b>
 │
-├<b>㊙ Language: <a href='https://www.python.org'>Python 3.9.4</a></b>
+├<b>Developer: <a href='https://t.me/dstamil_07'>@dstamil_07</a></b>
 │
-├<b>👨‍💻 Developer : <a href='https://t.me/PredatorHackerZ'>Pred∆tor</a></b>
-│
-├<b>🚸 Powered By : <a href='https://t.me/Moviesflixers_DL'>@HindiWebNetwork</a></b>
-│
-╰────────[Thank Yiu]───⍟
+╰────────[Thank You]───⍟
 
 __If issues persists contact my Master.__
 
