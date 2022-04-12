4ee2e123f9627794dfa2e975122743d3ea784b62
Dsbrodd <103419883+Dsbrodd@users.noreply.github.com>
2022-04-12 09:44:02 +0530

    Update forcesub.py

--- bot/database/forcesub.py
+++ bot/database/forcesub.py
@@ -13,7 +13,7 @@ async def ForceSub(bot: Client, cmd: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=cmd.from_user.id,
-                text="Sorry Son, You are Banned to use me. Contact my [Support Group](https://t.me/TeleRoid14).",
+                text="Sorry Son, You are Banned to use me. Contact my [Support Group](https://t.me/+eN6ukkVwZNIzYzU1).",
                 parse_mode="markdown",
                 disable_web_page_preview=True
             )
@@ -35,7 +35,7 @@ async def ForceSub(bot: Client, cmd: Message):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/TeleRoid14).\n\n**@TheTeleRoid**",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/mm_dev_channel).\n\n**@mm_dev_channel**",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
